2ff4b2a6b48791c4785b7e68b3563d9b20bbb1ff
dimaq12 <dimaq12@yandex.ru>
2017-04-15 21:52:06 +0300

    Initialize header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-  <h1>Hello</h1>
+  	<app-header></app-header>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+
 export default {
- 
+	data: function(){
+		return {
+
+		}
+	},
+	components: {
+		'app-header': Header
+	}
 }
 </script>
 
 <style lang="scss">
-
+body{
+	margin: 0;
+	padding: 0;
+	background-color: #eceff1;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,26 @@
-<template>
-	
+<template lang='pug'>
+	header.header
+		.header-wrap
+			.logo
 </template>
 <script>
+export default{
+
+}
 	
 </script>
-<style>
-	
+<style lang="scss">
+header{
+	width: 100%;
+	height: 60px;
+	background-color: #fff;
+	.logo{
+
+	}
+}
+.header-wrap{
+	max-width: 1200px;
+	margin: auto;
+	height: 100%;
+}
 </style>
